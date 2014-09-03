5f08f2a0059c3b0480f859b8d69c6f9c8f256ad2
Sebastian Johnsson <sebastian@agiley.se>
2014-09-03 10:47:14 +0200

    Upd

--- lib/namecheap/modules/dns.rb
+++ lib/namecheap/modules/dns.rb
@@ -24,14 +24,17 @@ module Namecheap
         params            =   generate_domain_params(domain)
         
         records.each_with_index do |record, index|
+          record          =   record.symbolize_keys!
+          
           if (record && record.has_key?(:hostname) && record.has_key?(:record_type) && record.has_key?(:address))
-            index    +=   1
+            index        +=   1
           
             params["HostName#{index}"]      =   record[:hostname]
             params["RecordType#{index}"]    =   record[:record_type]
             params["Address#{index}"]       =   record[:address]
-            params["MXPref#{index}"]        =   record.fetch(:mx_pref,  default_mx_pref)
-            params["TTL#{index}"]           =   record.fetch(:ttl,      default_ttl)
+            params["MXPref#{index}"]        =   record.fetch(:mx_pref,    default_mx_pref)
+            params["EmailType"]             =   record.fetch(:email_type, "") if [:mx, :mxe].include?(record[:record_type].downcase.to_sym)
+            params["TTL#{index}"]           =   record.fetch(:ttl,        default_ttl)
           end
         end
         
